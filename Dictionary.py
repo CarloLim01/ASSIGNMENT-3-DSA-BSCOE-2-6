5976d52063bd2b8b95c6173885f845547f0cca35
unknown <carlolim543@gmail.com>
2022-11-12 19:55:27 -0800

    Contact Tracing (Finalize, Design and Fixed some errors)

--- Dictionary.py
+++ Dictionary.py
@@ -38,57 +38,97 @@
 # Phone number: 1234567890What do you want to do? (1-3): 3
 # Exit? n
 
+#\033[1;35;1m  \033[0m
+
+#\033[1;3m  \033[0m
+
+#\033[1;255;1m  \033[0m
+
+import time
+
 user = []
 UserInfo = {}
-menu = ['1','2','3']
+
 
 choice = ""
-print("WELCOME TO CONTACT TRACING")
+time.sleep(2)
+print("\033[1;255;1m--------------- WELCOME TO CONTACT TRACING ---------------\033[0m\n")
 while(choice != "3"):
-    print("Menu:  ")
-    print("Option 1 — Check and Add your contact information")
-    print("Option 2 — Search for your contact information")
-    print("Option 3 — Exit the contact tracing")
-
-    choice = input("What do you want to do? (1-3):  ")
+    time.sleep(1)
+    print("     \n\033[1;255;3mMenu:\033[0m  ")
+    print("     \033[1;35;1mOption 1\033[0m — Check and Add your Contact Information")
+    print("     \033[1;35;1mOption 2\033[0m — Search for your Contact Information")
+    print("     \033[1;35;1mOption 3\033[0m — Exit the Contact Tracing")
+    print("\n\n\033[1;255;1m----------------------------------------------------------\033[0m\n")
+    time.sleep(2)
+    choice = input("\n\033[1;3mWhat do you want to do? (1-3):\033[0m  ")
     
     if choice == '1':
-        print("Check first if the user exist!")
+        print("\n\n\033[1;255;1m----------------------------------------------------------\033[0m\n")
+        time.sleep(2)
+        print("\n\033[1;3mCheck first if the user exist!\033[0m")
         Out = list(UserInfo.values())
-        Name = input("Full name: ")
+        Name = input("\n\033[1;35;1m    Full name:\033[0m ")
 
         if Name in Out:
-            print("This name already exists!")
+            print("\n\033[1;255;1mThis name already exists!\033[0m")
+            print("\n\n\033[1;255;1m----------------------------------------------------------\033[0m\n")
 
         else:
-            print("This name is not yet registered!")
-            print("ADD A NEW RECORD")
-            UserInfo["name"] = input("Full name:  ")
-            UserInfo["age"] = input("Age:  ")
-            UserInfo["address"] = input("Address:  ")
-            UserInfo["number"] = input("Phone number:  ")
-            UserInfo["school"] = input("School:  ")
-            UserInfo["email"] = input("E-mail:  ")
-            UserInfo["bday"] = input("Birthday:  ")
+            print("\n\033[1;255;1mWait a few seconds to Check your Record.\033[0m")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            print(".")
+            time.sleep(2)
+            print("\n\033[1;31;1mThis name is not yet registered!\033[0m\n")
+            print("\n\033[1;255;1m----------------------------------------------------------\033[0m\n")
+            time.sleep(2)
+            print("\n\033[1;255;1mADD A NEW RECORD\033[0m")
+            time.sleep(1)
+            UserInfo["\n\033[1;35;1mName\033[0m"] = input("\n\033[1;35;1m     Enter your Full name:\033[0m  ")
+            UserInfo["\033[1;35;1mAge\033[0m"] = input("\033[1;35;1m     Enter your Age:\033[0m  ")
+            UserInfo["\033[1;35;1mAddress\033[0m"] = input("\033[1;35;1m     Enter your Address:\033[0m  ")
+            UserInfo["\033[1;35;1mNumber\033[0m"] = input("\033[1;35;1m     Enter your Phone number:\033[0m  ")
+            UserInfo["\033[1;35;1mSchool\033[0m"] = input("\033[1;35;1m     Enter your School:\033[0m  ")
+            UserInfo["\033[1;35;1mEmail\033[0m"] = input("\033[1;35;1m     Enter your E-mail:\033[0m  ")
+            UserInfo["\033[1;35;1mBirthday\033[0m"] = input("\033[1;35;1m     Enter your Birthday:\033[0m  ")
             user.append(UserInfo)
-            print("Record Saved!")
+            time.sleep(2)
+            print("\n\033[1;255;1mWait a few seconds to Save your Record.\033[0m")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            print(".")
+            time.sleep(2)
+            print("\n\033[1;255;1mRecord Saved!\033[0m")
+            print("\n\n\033[1;255;1m----------------------------------------------------------\033[0m\n")
 
     if choice == '2':
-        print("SEARCH USER")
+        print("\n\n\033[1;255;1m----------------------------------------------------------\033[0m\n")
+        time.sleep(2)
+        print("\n\033[1;255;1mSearch for your Contact Information\033[0m")
         check = list(UserInfo.values())
-        find = input("Full name: ")
+        time.sleep(1)
+        find = input("\n\033[1;35;1m    Full name:\033[0m   ")
 
         if find in check:
             Out = None
             for key in choice:
                 for key in UserInfo:
+                    
                     print(key, ":", UserInfo[key])
 
         else:
-            print("This name is not yet registered!")
+            print("\n\033[1;31;1mThis name is not yet registered!\033[0m")
+            print("\n\n\033[1;255;1m----------------------------------------------------------\033[0m\n")
 
     if choice == '3':
-        print(f"THANK YOU FOR USING CONTACT TRACING")
+        print("\n\n\033[1;255;1m---------- THANK YOU FOR USING CONTACT TRACING! ----------\033[0m\n")
         exit()
-    else:
-        print("Input is invalid!")
+
+
